1301fa3162d9a0e9fd7d58c17f2cb0211a8f0eb6
Bishow Thapa <bishowthapa33@gmail.com>
2023-01-02 16:48:00 +0545

    added home app

--- crud_api/crud_api/settings.py
+++ crud_api/crud_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home',
     'rest_framework',
     'crud_api',
     'django.contrib.admin',
@@ -118,6 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# added static
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
