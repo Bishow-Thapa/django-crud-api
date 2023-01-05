62ec303e267d145b2c2330aaaa090a09b56ef708
Bishow Thapa <bishowthapa33@gmail.com>
2023-01-05 21:08:13 +0545

    added django-cors-headers

--- crud_api/crud_api/settings.py
+++ crud_api/crud_api/settings.py
@@ -41,9 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,3 +135,10 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# django-cors-headers
+
+CORS_ALLOWED_ORIGINS = [
+    "http://bishowthapa.com.np",
+    "http://localhost:3000",
+]
